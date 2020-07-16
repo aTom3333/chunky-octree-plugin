1f66e3cae3629aa5a378e6ad6758c9f41bcd45fb
Thomas Ferrand <a.tom.hic3@gmail.com>
2020-07-16 05:25:42 +0200

    Add DynamicByteArray and reorganize

---
+++ src/main/java/dev/ferrand/chunky/octree/utils/DynamicByteArray.java
@@ -0,0 +1,117 @@
+package dev.ferrand.chunky.octree.utils;
+
+import java.util.ArrayList;
+
+public class DynamicByteArray {
+    private final ArrayList<byte[]> data = new ArrayList<>();
+    private long size;
+    private long capacity;
+
+    private static final int FULL_ARRAY_SHIFT = 22;
+    private static final long MAX_ARRAY_SIZE = 1 << FULL_ARRAY_SHIFT;
+    private static final long SUB_ARRAY_MASK = MAX_ARRAY_SIZE - 1;
+    private static final long FULL_ARRAY_MASK = ~SUB_ARRAY_MASK;
+
+    public DynamicByteArray(long initialCapacity) {
+        this.capacity = initialCapacity;
+        while(initialCapacity > MAX_ARRAY_SIZE) {
+            data.add(new byte[(int) MAX_ARRAY_SIZE]);
+            initialCapacity -= MAX_ARRAY_SIZE;
+        }
+        data.add(new byte[(int) initialCapacity]);
+        size = 0;
+    }
+
+    public DynamicByteArray() {
+        this(64);
+    }
+
+    private void grow(long minNewCapacity) {
+        if(data.size() > 2) {
+            // Add whole array
+            data.add(new byte[(int) MAX_ARRAY_SIZE]);
+            capacity += MAX_ARRAY_SIZE;
+        } else if(data.size() == 2) {
+            // Grow the second array or add a third one
+            if(data.get(1).length == MAX_ARRAY_SIZE) {
+                // Add array
+                data.add(new byte[(int) MAX_ARRAY_SIZE]);
+                capacity += MAX_ARRAY_SIZE;
+            } else {
+                // We have an array and a half, we'll add one half to make 2 whole arrays
+                byte[] newArray = new byte[(int) MAX_ARRAY_SIZE];
+                System.arraycopy(data.get(1), 0, newArray, 0, (int) (size & SUB_ARRAY_MASK));
+                data.set(1, newArray);
+                capacity = 2 * MAX_ARRAY_SIZE;
+            }
+        } else {
+            // Grow the only array or add a second one
+            if(capacity == MAX_ARRAY_SIZE) {
+                data.add(new byte[(int) (MAX_ARRAY_SIZE/2)]);
+                capacity += MAX_ARRAY_SIZE / 2;
+            } else {
+                long newCapacity = (long)Math.floor(capacity * 1.5); // compute new capacity
+                newCapacity = Math.min(newCapacity, MAX_ARRAY_SIZE); // Don't grow over MAX_ARRAY_SIZE...
+                newCapacity = Math.max(newCapacity, minNewCapacity); // Unless minNewCapacity is over MAX_ARRAY_SIZE
+                long newArraySize = Math.min(newCapacity, MAX_ARRAY_SIZE);
+                byte[] newArray = new byte[(int) newArraySize];
+                System.arraycopy(data.get(0), 0, newArray, 0, (int) (size & SUB_ARRAY_MASK));
+                data.set(0, newArray);
+                capacity = newCapacity;
+                if(newCapacity > newArraySize) {
+                    data.add(new byte[(int) (MAX_ARRAY_SIZE/2)]);
+                    capacity = MAX_ARRAY_SIZE * 3 / 2;
+                }
+            }
+        }
+    }
+
+    public void ensureCapacity(long newCapacity) {
+        while(newCapacity > capacity) {
+            grow(newCapacity);
+        }
+    }
+
+    public byte get(long index) {
+        return data.get((int) (index >>> FULL_ARRAY_SHIFT))[(int) (index & SUB_ARRAY_MASK)];
+    }
+
+    public void set(long index, byte value) {
+        data.get((int) (index >>> FULL_ARRAY_SHIFT))[(int) (index & SUB_ARRAY_MASK)] = value;
+    }
+
+    public void pushBack(byte value) {
+        ensureCapacity(size+1);
+        set(size, value);
+        ++size;
+    }
+
+    public void writeElems(byte[] values, long fromArray, int fromValues, int count) {
+        long toArray = fromArray + count;
+        // Use array copy to efficiently add several elements
+        ensureCapacity(toArray);
+        --toArray;
+        long fullArrayIndex;
+        while((fullArrayIndex = (fromArray >>> FULL_ARRAY_SHIFT)) != (toArray >>> FULL_ARRAY_SHIFT)) {
+            // Copy left side when overlapping an edge
+            long to = (fullArrayIndex + 1) * MAX_ARRAY_SIZE - 1;
+            int subCount = (int) (to - fromArray + 1);
+            System.arraycopy(values, fromValues, data.get((int) fullArrayIndex), (int) (fromArray & SUB_ARRAY_MASK), subCount);
+            fromValues += subCount;
+            fromArray += subCount;
+        }
+
+        // Copy the final part (and only when not overlapping an edge
+        System.arraycopy(values, fromValues, data.get((int) fullArrayIndex), (int) (fromArray & SUB_ARRAY_MASK), (int) (toArray - fromArray + 1));
+
+        size = Math.max(size, toArray+1);
+    }
+
+    public void addElems(byte[] values, int count) {
+        writeElems(values, size, 0, count);
+    }
+
+    public long getSize() {
+        return size;
+    }
+}
---
+++ src/test/java/dev/ferrand/chunky/octree/utils/DynamicByteArrayTest.java
@@ -0,0 +1,59 @@
+package dev.ferrand.chunky.octree.utils;
+
+import dev.ferrand.chunky.octree.utils.DynamicByteArray;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DynamicByteArrayTest {
+
+    @Test
+    public void basicWriteRead() {
+        DynamicByteArray array = new DynamicByteArray();
+        for(int i = 0; i < 50; ++i) {
+            array.pushBack((byte) i);
+        }
+
+        assertEquals(50, array.getSize());
+
+        for(int i = 0; i < 50; ++i) {
+            assertEquals(i, array.get(i));
+        }
+    }
+
+    @Test
+    public void bigWriteRead() {
+        DynamicByteArray array = new DynamicByteArray();
+        long size = (1 << 24);
+        for(long i = 0; i < size; ++i) {
+            array.pushBack((byte) (i & 0x7F));
+        }
+
+        assertEquals(size, array.getSize());
+
+        for(long i = 0; i < size; ++i) {
+            assertEquals((byte) (i & 0x7F), array.get(i));
+        }
+    }
+
+    @Test
+    public void bigWriteElems() {
+        DynamicByteArray array = new DynamicByteArray();
+        long size = (1 << 24);
+        long offset = 50;
+
+        byte[] value = new byte[(int) size];
+        for(long i = 0; i < size; ++i) {
+            value[(int) i] = (byte) (i & 0x7F);
+        }
+
+        array.writeElems(value, offset, 0, (int) size);
+
+        assertEquals(offset + size, array.getSize());
+
+        for(long i = 0; i < size; ++i) {
+            assertEquals((byte) (i & 0x7F), array.get(offset + i));
+        }
+    }
+
+}
